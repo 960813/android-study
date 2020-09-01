b197b6f3ea46042ea5199c41fb5d53375af5a3fe
Keriel <keriel@jupiterflow.com>
2020-09-01 10:38:44 +0900

    :sparkles: 기본 생성자 누락 case

--- java-study/1week/src/StudentEx.java
+++ java-study/1week/src/StudentEx.java
@@ -1,8 +1,15 @@
+// 1. 이 프로그램을 실행하면 오류가 발생한다. 해결 방법은?
 class Student {
+    // Field
     private String name;
     private int age;
 
+    // 해결: 기본 생성자 추가
+    public Student() {
 
+    }
+
+    // Constructor
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
@@ -11,7 +18,7 @@ class Student {
 
 public class StudentEx {
     public static void main(String[] args) {
-        Student stu = new Student("Kim", 20);
-        System.out.println("stu : " + stu);
+        Student stu = new Student(); // 기본 생성자가 정의되지 않았는데 호출함.
+        Student stu1 = new Student("홍길동", 24);
     }
 }
