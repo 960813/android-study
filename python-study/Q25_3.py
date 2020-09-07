4bed18f3a4660dcb44dc8312cd287418fb7a971d
Keriel <keriel@jupiterflow.com>
2020-09-07 12:55:40 +0900

    :sparkles: 모두의 파이썬 Q25.3 풀이

---
+++ python-study/Q25_3.py
@@ -0,0 +1,31 @@
+def uniqe(L):
+    uniqe_res = []
+    for item in L:
+        if item not in uniqe_res:
+            uniqe_res.append(item)
+
+    return uniqe_res
+
+
+def common(L1, L2):
+    uniqe_l1 = uniqe(L1)
+    uniqe_l2 = uniqe(L2)
+    len_l1 = len(uniqe_l1)
+    len_l2 = len(uniqe_l2)
+
+    if len_l1 != len_l2:
+        return False
+
+    if len_l1 < len_l2:
+        uniqe_l1, uniqe_l2 = uniqe_l2, uniqe_l1
+
+    for item_l1 in uniqe_l1:
+        if item_l1 not in uniqe_l2:
+            return False
+
+    return True
+
+
+lst1 = [1, 1, 1]
+lst2 = [1]
+print(common(lst1, lst2))
