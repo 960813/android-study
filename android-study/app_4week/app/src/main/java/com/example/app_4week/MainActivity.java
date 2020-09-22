c37e11784e830e0b654a9f1fa8657db94c110cb5
Keriel <keriel@jupiterflow.com>
2020-09-23 01:02:49 +0900

    :sparkles: 안드로이드(1) 4주차 1차시 학습

--- android-study/app_4week/app/src/main/java/com/example/app_4week/MainActivity.java
+++ android-study/app_4week/app/src/main/java/com/example/app_4week/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.app_4week;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
+    ScrollView scrView;
+    ImageView imageView;
+    BitmapDrawable bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrView = findViewById(R.id.scrview);
+        imageView = findViewById(R.id.imgview);
+        scrView.setHorizontalScrollBarEnabled(true);
+
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image01);
+
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+
+    public void btnImageConvertClick(View v) {
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image02);
+
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
     }
 }
