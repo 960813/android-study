1dc1777bfe14719790509880f22a7214248f7fdc
Keriel <keriel@jupiterflow.com>
2020-09-29 02:43:28 +0900

    :sparkles: 파이썬(2) 4주차 학습 완료

--- python-study/4week.py
+++ python-study/4week.py
@@ -1,3 +1,6 @@
+##
+# 4주차 진도
+##
 class Circle(object):  # 클래스 생성 선언문
     def __init__(self):
         self.radius = 0  # 프로퍼티
@@ -7,6 +10,13 @@ class Circle(object):  # 클래스 생성 선언문
 
     def get_radius(self):
         return self.radius
+
+
+one_circle = Circle()
+another_circle = Circle()
+
+one_circle.change_radius(4)
+print(one_circle.get_radius())
 
 
 ##
@@ -44,3 +54,30 @@ class Door(object):
 
     def get_area(self):
         return self.width * self.height
+
+
+##
+# 셀프 체크 31.4
+##
+class Door(object):
+    def __init__(self):
+        self.width = 1
+        self.height = 1
+        self.open = False
+
+    def change_state(self):
+        self.open = not self.open
+
+    def scale(self, factor):
+        self.height *= factor
+        self.height *= factor
+
+    # 셀프 체크 외 추가 메서드
+    def get_size(self):
+        return self.width * self.height
+
+
+square_door = Door()
+square_door.change_state()
+square_door.scale(3)
+print(square_door.get_size())
