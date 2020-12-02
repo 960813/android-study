13fcd1b0821800a4ba41295b2c216c08e73cf740
JIN TAEYANG <sun@jupiterflow.com>
2020-12-02 18:05:21 +0900

    :sparkles: 자료구조실습 13주차 과제

--- data-structure-study/13week/13week_assignment_1905096_진태양.c
+++ data-structure-study/13week/13week_assignment_1905096_진태양.c
@@ -18,6 +18,7 @@ ListNode* insert_first(ListNode* head, element value)
     ListNode* p = (ListNode*)malloc(sizeof(ListNode));
     p->data = value;
     p->link = head;
+    
     head = p;
     return head;
 }
@@ -69,11 +70,43 @@ int main(void)
         print_list(head);
     }
 
-    for (int i = 0; i < 5; i++) {
-        head = delete_first(head);
-        print_list(head);
-    }
+    // for (int i = 0; i < 5; i++) {
+    //     head = delete_first(head);
+    //     print_list(head);
+    // }
 
-    
+    // 13주차 과제
+
+    // 신규 데이터 삽입 with insert()
+    int insert_pos = -1;
+    element insert_value = -1;
+
+    printf("신규 추가할 위치가 몇번째 인가요?(0부터 시작) ");
+    scanf("%d", &insert_pos);
+
+    printf("신규 추가할 데이터는 무엇인가요?(정수)");
+    scanf("%d", &insert_value);
+
+    ListNode* pre = head;
+    for (int i = 0; i < insert_pos - 1; i++) {
+        pre = pre->link;
+    }
+    head = insert(head, pre, insert_value);
+    print_list(head);
+
+    // 기존 데이터 삭제 with delete()
+    int delete_pos = -1;
+
+    printf("몇번째 데이터를 삭제하시겠습니까?(0부터 시작) ");
+    scanf("%d", &delete_pos);
+
+    pre = head;
+    for (int i = 0; i < delete_pos - 1; i++) {
+        pre = pre->link;
+    }
+    head = delete (head, pre);
+
+    print_list(head);
+
     return 0;
 }
